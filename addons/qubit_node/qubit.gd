1ddaff7a394a93fa614a23d7039fb3ee022c64ab
estebanzapirain <estebanzapirain@gmail.com>
2018-10-25 21:13:59 -0300

    Update qubit.gd

--- addons/qubit_node/qubit.gd
+++ addons/qubit_node/qubit.gd
@@ -1,9 +1,10 @@
 tool
 extends Node
 
-export var p0 = 0
-export var p1 = 0
+export var qstate = [1.0 0.0] setget qstate_set, qstate_get
 
-func _ready():
-	p0 = 0
-	p1 = 1
+func qstate_set(state)):
+	qstate = state
+
+func qstate_get():
+	return qstate
