7d040b7a334b661e82b65d0ab18904ea50838941
estebanzapirain <estebanzapirain@gmail.com>
2018-10-29 11:20:14 -0300

    Add files via upload

---
+++ src/addons/qubit_node/qubit.gd
@@ -0,0 +1,13 @@
+tool
+extends Node
+
+export var qstate = [1.0,0.0] setget qstate_set, qstate_get
+
+func qstate_set(state):
+	qstate = state
+
+func qstate_get():
+	return qstate
+	
+func qstate_show():
+	print(qstate)
